592256ac6698cbf67016eab77ba09e48cb4dc1d2
vvilli <hharriswilliam@gmail.com>
2021-04-07 23:50:55 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const operators = document.querySelectorAll(".buttons");
+const numbers = document.querySelectorAll(".numberBtns");
+
+// const [key, value] = numbers
+// console.log(key, value)
+
+// each button sends string to textarea input = operation = function?
+// responsive to a keypad & keyboard numbers
