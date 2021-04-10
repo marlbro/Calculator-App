97bdbfe6e7d417c8b092fb090205a6dec3000952
vvilli <hharriswilliam@gmail.com>
2021-04-10 02:37:54 -0500

    SCRAPPED to write most in JavaScript
    having elements defined in HTML isn't as flexible or has potential to scale compared to jS, so i'm rewriting

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-const operators = document.querySelectorAll(".buttons");
-const numbers = document.querySelectorAll(".numberBtns");
+"use strict";
+
+let calc_buttons = [];
+
+const clear = {
+  name: "delete",
+  symbol: "CLEAR",
+  formula: false,
+  type: "key",
+};
+
+const multiply = {
+  name: "multiplication",
+  symbol: "x",
+  formula: "*",
+  type: "operator",
+};
+
+const add = {
+  name: "addition",
+  symbol: "+",
+  formula: "+",
+  type: "operator",
+};
+
+const divide = {
+  name: "division",
+  symbol: "÷",
+  formula: "÷",
+  type: "operator",
+};
+
+const subtract = {
+  name: "subtraction",
+  symbol: "-",
+  formula: "-",
+  type: "operator",
+};
+
+calc_buttons.push(clear, multiply, add, subtract, divide);
+
+console.log(calc_buttons);
 
 // const [key, value] = numbers
 // console.log(key, value)
 
+const btns_per_row = 4;
+
+calc_buttons.forEach((button, index) => {});
+
 // each button sends string to textarea input = operation = function?
+
+/*
+
+const subtract = function (oper) {};
+const divide = function (oper) {};
+const multiply = function (oper) {};
+
+*/
 // responsive to a keypad & keyboard numbers
